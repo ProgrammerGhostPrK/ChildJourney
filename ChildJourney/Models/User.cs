c3107652e4d43898e3298592f3f13cef2b6b281a
ProgrammerGhostPrK <2202630frederiks@zuyd.nl>
2023-12-06 14:50:32 +0100

    User View

--- ChildJourney/Models/User.cs
+++ ChildJourney/Models/User.cs
@@ -17,21 +17,21 @@ namespace ChildJourney.Models
         [Required]
         public ICollection<User_Animal>? UserAnimals { get; set;}
         [Required]
-        public ICollection<User_Clothing> UserClothing { get; set;}
+        public ICollection<User_Clothing>? UserClothing { get; set;}
         [Required]
-        public ICollection<User_Decoration> UserDeco { get; set; }
+        public ICollection<User_Decoration>? UserDeco { get; set; }
         [Required]
         public ICollection<User_Reward>? UserRewards { get; set; }
         [Required]
-        public ICollection<User_BodyPart> UserBosyParts { get; set; }
+        public ICollection<User_BodyPart>? UserBosyParts { get; set; }
         [Required]
         public ICollection<Mood>? Moods { get; set; }
         public int UnlockedIslands { get; set; }
         [Required]
-        public Outfit Outfit { get; set; }
+        public Outfit? Outfit { get; set; }
         [Required]
-        public Body Body { get; set; }
+        public Body? Body { get; set; }
         [Required]
-        public House House { get; set; }
+        public House? House { get; set; }
     }
 }
