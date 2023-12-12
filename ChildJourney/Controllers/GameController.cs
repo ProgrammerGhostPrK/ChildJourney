3f8a1fea944bb624f218c30e45e528e98284c1ef
ProgrammerGhostPrK <2202630frederiks@zuyd.nl>
2023-12-12 11:17:43 +0100

    Created function for dialogue adding and added functions to show dialogue in introdcution

---
+++ ChildJourney/Controllers/GameController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChildJourney.Controllers
+{
+    public class GameController : Controller
+    {
+
+        public GameController()
+        {
+        }
+
+        public ActionResult Introduction() 
+        { 
+            return View();
+        }
+
+        public List<string> AddDialogue(string Text, List<string> list)
+        {
+            list.Add(Text);
+            return list;
+        }
+
+        public List<string> RemoveDialogue(List<string> list)
+        {
+            list.RemoveAt(0);
+            return list;
+        }
+    }
+}
