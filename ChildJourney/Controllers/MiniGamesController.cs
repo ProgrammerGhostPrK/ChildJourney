9a7947570856bdcf43db2790064274c9ce843b7f
ProgrammerGhostPrK <2202630frederiks@zuyd.nl>
2024-01-09 14:58:32 +0100

    2 minigames added + Removed AddCoins(10) from store

---
+++ ChildJourney/Controllers/MiniGamesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChildJourney.Controllers
+{
+    public class MiniGamesController : Controller
+    {
+        public IActionResult BoatSteering()
+        {
+            return View();
+        }
+        public IActionResult BirdFlying()
+        {
+            return View();
+        }
+    }
+}
