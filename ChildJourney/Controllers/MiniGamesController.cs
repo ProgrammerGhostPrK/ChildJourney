216c7b6e1f0e646752b4c4acdcc8be9a0b4dbd0f
ProgrammerGhostPrK <2202630frederiks@zuyd.nl>
2024-01-10 14:55:44 +0100

    Players can now win minigames

--- ChildJourney/Controllers/MiniGamesController.cs
+++ ChildJourney/Controllers/MiniGamesController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ChildJourney.Data;
+using ChildJourney.Models;
+using Newtonsoft.Json;
 
 namespace ChildJourney.Controllers
 {
     public class MiniGamesController : Controller
     {
-        public IActionResult BoatSteering()
+        private readonly Database _context;
+
+        public MiniGamesController(Database context)
+        {
+            _context = context;
+        }
+        public HomeController HomeController()
+        {
+            var Hc = new HomeController(_context);
+            return Hc;
+        }
+        public IActionResult BoatSteering(int? Id)
         {
             return View();
         }
         public IActionResult BirdFlying()
         {
-            return View();
+            return View(HomeController().AdminViewModel());
         }
     }
 }
